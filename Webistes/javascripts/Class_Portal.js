8482062cb7c10406ef1c4dd3b32d9fb7d8d50db1
Sam Herwif <Sahe2032@colorado.edu>
2020-09-10 13:47:32 -0600

    Link to Lab

--- Webistes/javascripts/Class_Portal.js
+++ Webistes/javascripts/Class_Portal.js
@@ -1,14 +1,1 @@
 
-
-// var labNav= document.getElementById('home');
-console.log(document.querySelector('.topnav'))
-
-document.querySelector('.topnav').addEventListener('click', event => {
-    console.log('yep')
-})
-
-
-// labNav.addEventListener('click', function()
-// {
-//   console.log('yep')
-// });
