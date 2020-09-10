dcb9c82523a470093ee5db2a9ff3f7a0793b669b
Sam Herwif <Sahe2032@colorado.edu>
2020-09-10 13:08:14 -0600

    Lab page (broken)

---
+++ Webistes/javascripts/Class_Portal.js
@@ -0,0 +1,14 @@
+
+
+// var labNav= document.getElementById('home');
+console.log(document.querySelector('.topnav'))
+
+document.querySelector('.topnav').addEventListener('click', event => {
+    console.log('yep')
+})
+
+
+// labNav.addEventListener('click', function()
+// {
+//   console.log('yep')
+// });
