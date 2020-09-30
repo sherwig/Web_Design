494ccf0e6c1f6762f4efeb140841cb28502af7ec
Sam Herwif <Sahe2032@colorado.edu>
2020-09-30 16:06:24 -0600

    lab4-update

--- Webistes/javascripts/Lab 4.js
+++ Webistes/javascripts/Lab 4.js
@@ -1,8 +1,58 @@
+//Need a second event
 
-$( document ).ready(function() 
+$( document ).ready(function()
 {
 
-  $("#Pgraph").css("backgroundColor", "yellow");
+  // $("#Pgraph").css("backgroundColor", "yellow");
+  var para= $("#Pgraph");
+  para.css("backgroundColor", "yellow");
+  // $("loop1").css("backgroundColor", "yellow");
+
+  var image=$('#loop1');
+  image.hide();
+  image.width(700);
+  image.height(700);
+
+  var clicker= $('#clicker');
+
+
+  var click2=$('#cool');
+  click2.hide();
+
+  clicker.click(function() {
+  image.fadeIn( "slow", function() {
+    // Animation complete
+  });
+  click2.fadeIn( "slow", function() {
+    // Animation complete
+  });
+});
+
+
+
+  var image2=$('#loop2');
+  image2.hide();
+  image2.width(700);
+  image2.height(700);
+
+  var video=$('#video');
+  video.hide();
+
+
+  click2.click(function() {
+  image2.fadeIn( "slow", function() {
+    // Animation complete
+  });
+
+  video.fadeIn( "slow", function() {
+    // Animation complete
+  });
+
+});
+
+
+  // $('#loop1').width(700); // Units are assumed to be pixels
+  // $('#loop1').height(700);
 
 
 });
